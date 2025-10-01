4776915f4c2e08134024afbf3a0724eaf1b06119
schorts99 <schorts99@gmail.com>
2025-10-01 17:21:35 +0000

    fix: `PrimitiveTypesToFirestoreFormmater`.
    Now the Date are also translated into Timestamp.

--- src/primitive-types-to-firestore-formatter.ts
+++ src/primitive-types-to-firestore-formatter.ts
@@ -1,9 +1,13 @@
+import { Timestamp } from "firebase/firestore";
 import { Geohash, geohashForLocation } from "geofire-common";
 import { CoordinatesValue, PascalCamelToSnake } from "@schorts/shared-kernel";
 
 export class PrimitiveTypesToFirestoreFormmater {
   static format<Entity>(entity: Entity) {
-    return this.formatCoordinates<Entity>(entity);
+    return {
+      ...this.formatCoordinates<Entity>(entity),
+      ...this.formatDates<Entity>(entity),
+    };
   }
 
   static formatCoordinates<Entity>(entity: Entity): Record<string, Geohash> {
@@ -20,5 +24,21 @@ export class PrimitiveTypesToFirestoreFormmater {
     }
 
     return geoData;
+  }
+
+  static formatDates<Entity>(entity: Entity): Record<string, Timestamp> {
+    const formattedDates: Record<string, Timestamp> = {};
+
+    for (const key in entity) {
+      if (Object.prototype.hasOwnProperty.call(entity, key)) {
+        const value = (entity as any)[key];
+
+        if (value instanceof Date) {
+          formattedDates[PascalCamelToSnake.format(key)] = Timestamp.fromDate(value);
+        }
+      }
+    }
+
+    return formattedDates;
   }
 }
